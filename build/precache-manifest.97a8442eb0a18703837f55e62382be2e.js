f0a89a75e1bab8501889c19bc2b69169d8c7d784
LEANDRO DA SILVA <ld_silva13@hotmail.com>
2020-09-09 22:19:15 -0300

    build test

---
+++ build/precache-manifest.97a8442eb0a18703837f55e62382be2e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a399e9831f16431fd56bec73fc521b86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bf39edce89f72d42d6a",
+    "url": "/static/css/main.dd8541c7.chunk.css"
+  },
+  {
+    "revision": "89fbafcf663083fb0705",
+    "url": "/static/js/2.f56eca28.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f56eca28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bf39edce89f72d42d6a",
+    "url": "/static/js/main.d0bdcabe.chunk.js"
+  },
+  {
+    "revision": "1e32d2e2019888e1a0b2",
+    "url": "/static/js/runtime-main.65c8254e.js"
+  },
+  {
+    "revision": "9a22c7f5fc5ac77f30bacae8ceb623ba",
+    "url": "/static/media/book-24px.9a22c7f5.svg"
+  },
+  {
+    "revision": "19142cc51b8c074217df3d7499267bf0",
+    "url": "/static/media/search-black-18dp.19142cc5.svg"
+  }
+]);
