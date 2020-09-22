b2a6372e005eeafc4aa8ac30e46354f6c3686954
LEANDRO DA SILVA <ld_silva13@hotmail.com>
2020-09-22 20:09:49 -0300

    Adicionado classe para centralizar conteúdo

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import "./styles.css";
 
 export default function HomePage() {
   return (
-    <>
+    <div className="container-home">
       <Header />
       <Footer />
-    </>
+    </div>
   )
 }
