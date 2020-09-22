1c5cb50942aa9ff0721fac492f80fd9990f947de
LEANDRO DA SILVA <ld_silva13@hotmail.com>
2020-09-22 20:10:53 -0300

    - Adicionado placeholder nos inputs. - Adicionado Breadcrumb - Adicionado mensagem de erros via span

--- src/pages/RegisterPage/index.js
+++ src/pages/RegisterPage/index.js
@@ -7,6 +7,7 @@ import SessionStorage from "../../util/SessionStorage";
 import {authActions} from "../../store/authSlice";
 import logo from "../../assets/images/book-24px.svg";
 import Button from "../../components/Button";
+import Breadcrumbs from "@material-ui/core/Breadcrumbs";
 
 export default function RegisterPage() {
   const [isLoading, setIsLoading] = useState(false);
@@ -16,12 +17,13 @@ export default function RegisterPage() {
   const [passwordRetyped, setPasswordRetyped] = useState("");
   const dispatch = useDispatch();
   const history = useHistory();
+  const [errorMessage, setErrorMessage] = useState('');
 
   async function doRegister(evt) {
     evt.preventDefault();
 
     if (!isPasswordValid) {
-      alert("passwords don't match");
+      setErrorMessage("passwords don't match")
       return
     }
 
@@ -43,12 +45,22 @@ export default function RegisterPage() {
     })
     .catch(error => {
       if (error.response && error.response.data && error.response.data.error) {
-        alert(`${error.response.data.error}`)
+        setErrorMessage(`${error.response.data.error}`)
       } else {
-        alert(`An unexpected error occurred`)
+        setErrorMessage(`An unexpected error occurred`)
       }
     });
     setIsLoading(false);
+  }
+
+  function renderErrorMessage() {
+    if (!errorMessage) {
+      return null;
+    }
+    if (!isPasswordValid && password.length && passwordRetyped.length) {
+      return (<span className="error-message">passwords don't match</span>)
+    }
+    return (<span className="error-message">{errorMessage}</span>)
   }
 
   useEffect(() => {
@@ -63,44 +75,62 @@ export default function RegisterPage() {
         <h1 title="Dictionary">Dictionary</h1>
       </header>
       <main className="container-register">
+        <Breadcrumbs aria-label="breadcrumb">
+          <Link color="inherit" to="/">Home</Link>
+          <Link
+              color="textPrimary"
+              to="/register"
+              aria-current="page"
+          >
+            Register
+          </Link>
+        </Breadcrumbs>
         <form>
-          <section>
+          <section className="container-register">
             <h2>Register</h2>
-            <label htmlFor="input-email">E-mail:</label>
-            <input id="input-email"
-                   className="hover-effect"
-                   type="text"
-                   title="E-mail"
-                   required
-                   value={email}
-                   onChange={evt => setEmail(evt.target.value)}
-                   autoFocus />
+            <div>
+              <label htmlFor="input-email">E-mail:</label>
+              <input id="input-email"
+                     className="hover-effect"
+                     type="text"
+                     title="E-mail"
+                     placeholder="example@email.com"
+                     required
+                     value={email}
+                     onChange={evt => setEmail(evt.target.value)}
+                     autoFocus />
+            </div>
 
-            <label htmlFor="input-password">Password:</label>
-            <input id="input-password"
-                   className="hover-effect"
-                   type="password"
-                   title="Password"
-                   required
-                   value={password}
-                   onChange={evt => setPassword(evt.target.value)}
-                   autoFocus />
+            <div>
+              <label htmlFor="input-password">Password:</label>
+              <input id="input-password"
+                     className="hover-effect"
+                     type="password"
+                     title="Password"
+                     placeholder="password"
+                     required
+                     value={password}
+                     onChange={evt => setPassword(evt.target.value)}
+                     autoFocus />
+            </div>
 
-           <label htmlFor="input-password-retype">Retype password:</label>
-            <input id="input-password-retype"
-                   className="hover-effect"
-                   type="password"
-                   title="Retype password"
-                   required
-                   value={passwordRetyped}
-                   onChange={evt => setPasswordRetyped(evt.target.value)}
-                   autoFocus />
-            {
-              !isPasswordValid && password.length && passwordRetyped.length
-                ? <span className="invalid-password">passwords don't match</span> : null
-            }
-            <Link className="login-link" to="/login">I already have an account</Link>
+            <div>
+              <label htmlFor="input-password-retype">Retype password:</label>
+              <input id="input-password-retype"
+                     className="hover-effect"
+                     type="password"
+                     title="Retype password"
+                     placeholder="password"
+                     required
+                     value={passwordRetyped}
+                     onChange={evt => setPasswordRetyped(evt.target.value)}
+                     autoFocus />
+            </div>
+
+            {renderErrorMessage()}
+
             <Button onClick={doRegister} type="button" text={isLoading ? "...Loading" : "Register"} />
+            <Link className="login-link" to="/login">I already have an account</Link>
           </section>
         </form>
       </main>
